02828e5f5a900dc31e2d9a28578c69f305b4972a
赵海鹏 <9694526@qq.com>
2021-08-04 07:46:57 +0800

    feat:曝光埋点

--- src/main.js
+++ src/main.js
@@ -5,6 +5,33 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+
+const intersectionObserver = new IntersectionObserver(
+  function(entries) {
+    entries.forEach((entry) => {
+      // 当节点进入视窗
+      if (entry.isIntersecting) {
+        const ctm = entry.target.attributes["exposure-data"].value;
+        console.log("sendsa::",ctm)
+      }
+    });
+  },
+  {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5, // 不一定非得全部露出来  这个阈值可以小一点点
+  }
+);
+
+// 曝光埋点
+Vue.directive('exposure', {
+  // 当被绑定的元素插入到 DOM 中时……
+  bind(el) {
+    // 监听元素
+    intersectionObserver.observe(el)
+  }
+})
+
 new Vue({
   router,
   store,
---
+++ src/views/List.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <ul>
+      <li class="item" v-for="num in nums" :key="num" v-exposure="num" :exposure-data="num">{{num}}</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      nums:20
+    }
+  },
+  created(){
+
+  },
+  methods:{
+  }
+}
+</script>
+<style scoped>
+.item{
+  padding: 30px;
+  background-color: #ccc;
+  margin-bottom: 10px;
+  text-align: center;
+}
+</style>
